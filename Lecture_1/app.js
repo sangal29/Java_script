5637e7c951f9fb8830cf3736811e62c588c9e06b
sangal29 <sangalrishabh29@gmail.com>
2025-01-27 18:08:25 +0530

    lecture 1 complete

--- Lecture_1/app.js
+++ Lecture_1/app.js
@@ -1,1 +1,14 @@
     console.log("Hello world");
+    name = " anjali";
+    var student ={
+        fullName :"Rishabh Sangal",
+        class: 12,
+        pass: true,
+
+    };
+    console.log(student);
+    typeof student ;
+    console.log(student.pass);
+    console.log(student["pass"]);
+     student.class = student["class"] + 1;
+    console.log(student.class);
