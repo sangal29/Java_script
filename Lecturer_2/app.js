e0ea79776b036831faf01222d7a135b2f470ba66
sangal29 <sangalrishabh29@gmail.com>
2025-01-28 20:07:20 +0530

    second lecture

---
+++ Lecturer_2/app.js
@@ -0,0 +1,13 @@
+/* hey i am using
+ the js comment */
+
+
+ let value = promt("write the value")
+ if( value % 5 == 0 )
+ {
+    console.log("The number is multiple of 5");
+
+ }
+ else {
+    console.log("The number is not the multiple of 5 ");
+ }
